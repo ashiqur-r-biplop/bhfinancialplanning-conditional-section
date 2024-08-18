5dceb5ac6bc37a415b477436b2e5701177956265
ashiqur-r-biplop <biplops22@gmail.com>
2024-08-19 05:54:34 +0600

    added some code

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// 
+function setActive(element, contentId) {
+  const h1Elements = document.querySelectorAll(".option h1");
+  h1Elements.forEach((h1) => h1.classList.remove("active"));
+  const contentSections = document.querySelectorAll(".content-box .content");
+  contentSections.forEach((content) => (content.style.display = "none"));
+  element.classList.add("active");
+  document.getElementById(contentId).style.display = "flex";
+}
+document.addEventListener("DOMContentLoaded", function () {
+  setActive(document.querySelector(".option h1.active"), "option-1");
+});
